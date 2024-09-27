99f599d37e58bc45e77cc19f619ccc72687042c1
ApiOSDev <attup23@gmail.com>
2024-09-27 17:43:31 -0400

    Initial Commit

---
+++ CombineExample/CombineExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineExampleApp.swift
+//  CombineExample
+//
+//  Created by Ashesh Patel on 2024-09-27.
+//
+
+import SwiftUI
+
+@main
+struct CombineExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
